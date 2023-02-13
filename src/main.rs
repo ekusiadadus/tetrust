11a6e7034728acf56b68256e8a70e73280a33378
ekusiadadus <ekusiadadus@gmail.com>
2023-02-13 15:31:06 +0900

    feat: :fire: add init field

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    for y in 0..21 {
+        for x in 0..13 {
+            if x == 0 || x == 12 || y == 20 {
+                print!("[]")
+            } else {
+                print!(" .")
+            }
+        }
+        println!();
+    }
 }
