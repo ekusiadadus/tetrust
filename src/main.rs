faca5ec02fe65c02856b5c4a2ce299b94687a5da
ekusiadadus <ekusiadadus@gmail.com>
2023-02-15 04:18:05 +0900

    feat: :fire: add field_buf

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,21 @@ fn main() {
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
+    let block = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]];
+
+    let mut field_buf = field;
+
+    for y in 0..4 {
+        for x in 0..4 {
+            if block[y][x] == 1 {
+                field_buf[y + 8][x + 4] = 1;
+            }
+        }
+    }
+
     for y in 0..21 {
         for x in 0..13 {
-            if field[y][x] == 1 {
+            if field_buf[y][x] == 1 {
                 print!("[]");
             } else {
                 print!(" .");
