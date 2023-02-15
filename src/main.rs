e21d7d133d7ae0aeebd64ff47a7fc9730cc1bab8
ekusiadadus <ekusiadadus@gmail.com>
2023-02-15 13:16:24 +0900

    feat: :fire: show display and add getch-rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::{thread, time};
 
+const FIELD_WIDTH: usize = 11 + 2;
+const FIELD_HEIGHT: usize = 20 + 1;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+#[derive(Clone, Copy)]
 enum BlockKind {
     I,
     O,
@@ -34,8 +39,22 @@ struct Position {
     y: usize,
 }
 
+fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if y + pos.y >= FIELD_HEIGHT || x + pos.x >= FIELD_WIDTH {
+                return true;
+            }
+            if field[y + pos.y][x + pos.x] & BLOCKS[block as usize][y][x] == 1 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn main() {
-    let field = [
+    let field: Field = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -63,8 +82,12 @@ fn main() {
 
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    for _ in 0..5 {
+    for _ in 0..30 {
         let mut field_buf = field;
+
+        if !is_collision(&field, &pos, BlockKind::I) {
+            pos.y += 1;
+        }
 
         for y in 0..4 {
             for x in 0..4 {
@@ -74,11 +97,9 @@ fn main() {
             }
         }
 
-        pos.y += 1;
-
         println!("\x1b[H");
-        for y in 0..21 {
-            for x in 0..13 {
+        for y in 0..FIELD_HEIGHT {
+            for x in 0..FIELD_WIDTH {
                 if field_buf[y][x] == 1 {
                     print!("[]");
                 } else {
@@ -87,7 +108,7 @@ fn main() {
             }
             println!();
         }
-        thread::sleep(time::Duration::from_millis(1000));
+        thread::sleep(time::Duration::from_millis(100));
     }
 
     println!("\x1b[?25h");
