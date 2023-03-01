249fe62c2b6fb2ef28ff5e361135a31823769b6c
ekusiadadus <ekusiadadus@gmail.com>
2023-03-01 18:43:29 +0900

    chore: :fire: revert commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod block;
-mod game;
 use block::{BlockKind, BLOCKS};
-use game::{Game, Position, FIELD, FIELD_HEIGHT, FIELD_WIDTH};
 use getch_rs::{Getch, Key};
 use rand::{
     distributions::{Distribution, Standard},
@@ -10,12 +8,16 @@ use rand::{
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
-fn draw(Game { field, pos, block }: &Game) {
+const FIELD_WIDTH: usize = 11 + 2;
+const FIELD_HEIGHT: usize = 20 + 1;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+fn draw(field: &Field, pos: &Position, block: BlockKind) {
     let mut field_buf = field.clone();
 
     for y in 0..4 {
         for x in 0..4 {
-            if BLOCKS[*block as usize][y][x] == 1 {
+            if BLOCKS[block as usize][y][x] == 1 {
                 field_buf[y + pos.y][x + pos.x] = 1;
             }
         }
@@ -34,6 +36,11 @@ fn draw(Game { field, pos, block }: &Game) {
     }
 }
 
+struct Position {
+    x: usize,
+    y: usize,
+}
+
 fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
     for y in 0..4 {
         for x in 0..4 {
@@ -49,26 +56,56 @@ fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
 }
 
 fn main() {
+    let field = Arc::new(Mutex::new([
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]));
 
-    let game = Arc::new(Mutex::new(Game::new()));
-    }
-
+    let pos = Arc::new(Mutex::new(Position { x: 4, y: 0 }));
+    let block = Arc::new(Mutex::new(rand::random::<BlockKind>()));
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    draw(&game.lock().unwrap());
+    draw(
+        &field.lock().unwrap(),
+        &pos.lock().unwrap(),
+        *block.lock().unwrap(),
+    );
 
     {
-        let game = Arc::clone(&game);
+        let pos = Arc::clone(&pos);
+        let field = Arc::clone(&field);
+        let block = Arc::clone(&block);
 
         let _ = thread::spawn(move || loop {
             thread::sleep(time::Duration::from_millis(100));
-            let mut game = game.lock().unwrap();
+            let mut pos = pos.lock().unwrap();
+            let mut field = field.lock().unwrap();
+            let mut block = block.lock().unwrap();
             let new_pos = Position {
-                x: game.pos.x,
-                y: game.pos.y + 1,
+                x: pos.x,
+                y: pos.y + 1,
             };
-            if !is_collision(&field, &new_pos, game.block) {
-                game.pos = new_pos;
+            if !is_collision(&field, &new_pos, *block) {
+                *pos = new_pos;
             } else {
                 for y in 0..4 {
                     for x in 0..4 {
